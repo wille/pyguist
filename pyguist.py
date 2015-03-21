ed58742cec5c0904b489375c90c025c3a1c95e90
redpois0n <red@cmail.nu>
2015-03-21 23:08:22 +0100

    Small fixes

--- pyguist.py
+++ pyguist.py
@@ -10,18 +10,17 @@ with open("languages.yml") as f:
     langc = None
     
     for line in content:
-        if line.startswith("  - "):
+        if line.startswith("  - ."):
             extension = line.replace("  - .", "")
             extensions.append(extension)
         elif line.startswith(" "):
             continue
         else:
-            if not langc is None:
+            if not langc is None and not langc.lang is None:
                 langc.extensions = extensions
             line = line.replace(":", "").strip()
             if len(line) > 0:
                 langc = Language(line)
-                print("Creating language: " + line)
                 languages.append(langc)
                 extensions = []
             
